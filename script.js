afede1ed4fa1e1cc4f1fb5358d6a765aebad0773
Siva kumar sahoo <87701311+Sivakumarsahoo@users.noreply.github.com>
2022-01-10 03:12:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+setInterval(() => {
+   d=new Date();
+   htime=d.getHours();
+   mtime=d.getMinutes();
+   stime=d.getSeconds();
+   h=30*htime+mtime/2;
+   m=6*mtime;
+   s=6*stime;
+   document.querySelector("#second").style.transform= `rotate(${s}deg)`;
+   
+   document.querySelector("#hour").style.transform= `rotate(${h}deg)`;
+   
+  
+   document.querySelector("#minute").style.transform= `rotate(${m}deg)`;
+}, 1000);
+    
